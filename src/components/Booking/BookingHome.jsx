b92c71865ee7ad29bc118151029bf8e6515a5647
Adish <adishkerkar@gmail.com>
2023-09-02 03:22:20 +0530

    added logic to find and display service on booking page

--- src/components/Booking/BookingHome.jsx
+++ src/components/Booking/BookingHome.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BookingTime from './BookingTime'
 import BookinCalender from "./BookingCalender";
 import { useLocation } from 'react-router-dom';
@@ -7,20 +7,31 @@ import { useEffect } from 'react';
 import Bookingprovider from './Bookingprovider';
 import { SERVICES_DATA } from '../apiUrls';
 import axios from "axios";
+import { useParams } from 'react-router-dom';
+
 
 function BookingHome() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const provider = searchParams.get('provider');
-  const service = searchParams.get('service');
-  
+  const providerparams = searchParams.get('provider');
+  const serviceparams = searchParams.get('service');
+  const [servicedata,setServicedata] = useState(null);
+  const id = useParams();
+
   useEffect(() => {
     
     let getservicesdata = async()=>{
       let servicedata = await axios.get(SERVICES_DATA);
-      console.log(servicedata.data);
+      return servicedata.data.service_owners;
     }
-    getservicesdata();
+    getservicesdata().then((data)=>{
+      console.log(data);
+      let filterservice = data.find((elem)=>{
+        return elem.id==id.id
+      });
+      let servicename = filterservice.services.find((service)=>service.id==serviceparams);
+      setServicedata(servicename.name);
+    });
 
   }, [])
   
@@ -31,9 +42,9 @@ function BookingHome() {
     <h1 id='mainBookingHometxt'>Booking</h1>  
     <h3 id='AppointmentMaintxt'>Appointment</h3>
      <BookinCalender/>
-     <span id='SelectedServiceBooking'>{service}</span>
+     <span id='SelectedServiceBooking'>{servicedata}</span>
     <BookingTime/>
-    <Bookingprovider providerproperties={provider}/>
+    <Bookingprovider providerproperties={providerparams}/>
     </div>
   )
 }
