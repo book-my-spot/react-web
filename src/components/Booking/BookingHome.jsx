31ed35250d242ee49e5e2562d4b337664fe5b203
Adish <adishkerkar@gmail.com>
2023-09-02 02:43:35 +0530

    added functionality to get data from query string

--- src/components/Booking/BookingHome.jsx
+++ src/components/Booking/BookingHome.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import BookingTime from './BookingTime'
 import BookinCalender from "./BookingCalender";
+import { useLocation } from 'react-router-dom';
 import './BookingHome.css'
+import Bookingprovider from './Bookingprovider';
+
 
 function BookingHome() {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const provider = searchParams.get('provider');
+  const service = searchParams.get('service');
+
   return (
     <div>
     <h1 id='mainBookingHometxt'>Booking</h1>  
     <h3 id='AppointmentMaintxt'>Appointment</h3>
      <BookinCalender/>
-     <span id='SelectedServiceBooking'>Haircut</span>
+     <span id='SelectedServiceBooking'>{service}</span>
     <BookingTime/>
+    <Bookingprovider providerproperties={provider}/>
     </div>
   )
 }
---
+++ src/components/Booking/Bookingprovider.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Bookingprovider() {
+  return (
+    <div>Bookingprovider</div>
+  )
+}
+
+export default Bookingprovider
