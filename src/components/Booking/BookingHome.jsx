05b8c2031523799b0a599246572d5bcdf9dae5d9
Adish <adishkerkar@gmail.com>
2023-09-02 03:42:52 +0530

    added logic to display the provider on booking page

--- src/components/Booking/BookingHome.jsx
+++ src/components/Booking/BookingHome.jsx
@@ -5,7 +5,7 @@ import { useLocation } from 'react-router-dom';
 import './BookingHome.css'
 import { useEffect } from 'react';
 import Bookingprovider from './Bookingprovider';
-import { SERVICES_DATA } from '../apiUrls';
+import { SERVICES_DATA, BASE_URL } from '../apiUrls';
 import axios from "axios";
 import { useParams } from 'react-router-dom';
 
@@ -15,36 +15,56 @@ function BookingHome() {
   const searchParams = new URLSearchParams(location.search);
   const providerparams = searchParams.get('provider');
   const serviceparams = searchParams.get('service');
-  const [servicedata,setServicedata] = useState(null);
+  const [servicedata, setServicedata] = useState(null);
+  const [providerdata, setProviderdata] = useState(null);
   const id = useParams();
 
   useEffect(() => {
-    
-    let getservicesdata = async()=>{
+
+
+
+    let getservicesdata = async () => {
       let servicedata = await axios.get(SERVICES_DATA);
       return servicedata.data.service_owners;
     }
-    getservicesdata().then((data)=>{
-      console.log(data);
-      let filterservice = data.find((elem)=>{
-        return elem.id==id.id
+    getservicesdata().then((data) => {
+
+      let filterservice = data.find((elem) => {
+        return elem.id == id.id
       });
-      let servicename = filterservice.services.find((service)=>service.id==serviceparams);
+      let servicename = filterservice.services.find((service) => service.id == serviceparams);
       setServicedata(servicename.name);
     });
 
   }, [])
-  
+
+  useEffect(() => {
+    async function getServiceProviders() {
+      const response = await axios.get(`${BASE_URL}/getServiceProvider?service_owner_id=${id.id}`);
+      return response.data.service_providers.Items;
+    }
+
+    getServiceProviders().then((data) => {
+      let providerdetails = data.find((provider) => provider.name == providerparams);
+      setProviderdata(providerdetails);
+    });
+  }, [])
+
+
 
 
   return (
     <div>
-    <h1 id='mainBookingHometxt'>Booking</h1>  
-    <h3 id='AppointmentMaintxt'>Appointment</h3>
-     <BookinCalender/>
-     <span id='SelectedServiceBooking'>{servicedata}</span>
-    <BookingTime/>
-    <Bookingprovider providerproperties={providerparams}/>
+      <h1 id='mainBookingHometxt'>Booking</h1>
+      <h3 id='AppointmentMaintxt'>Appointment</h3>
+      <BookinCalender />
+      <span id='SelectedServiceBooking'>{servicedata}</span>
+      <BookingTime />
+      {providerdata?(
+        <Bookingprovider providerProperties={providerdata} />
+      ):(
+        <p>loading</p>
+      )}
     </div>
   )
 }
--- src/components/Booking/Bookingprovider.jsx
+++ src/components/Booking/Bookingprovider.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import maleServiceProvider from "../Images/maleServiceprovider.png";
+import femaleServiceProvider from "../Images/femaleServiceproviders.jpg";
 
-function Bookingprovider() {
+
+function Bookingprovider({ providerProperties }) {
+  console.log(providerProperties);
+  const providerName = providerProperties.name;
+  const image = providerProperties.image;
+  const gender = providerProperties.gender;
+  
   return (
-    <div>Bookingprovider</div>
+    <>
+      <div className="providersCardContainer" id='providersCardContainer'>
+        <div id='providersCardimgContainer'>
+          {image ? (
+            <img src={image} alt="" />
+          ) : (
+            gender === 'male' ? (
+              <img src={maleServiceProvider} />
+            ) : (
+              <img src={femaleServiceProvider} />
+            )
+          )}
+        </div>
+        <span>{providerName}</span>
+      </div>
+    </>
   )
 }
+
+
+Bookingprovider.propTypes = {
+    providerProperties: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    gender: PropTypes.string.isRequired
+  }).isRequired,
+};
+
 
 export default Bookingprovider
