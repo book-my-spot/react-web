9d2f5a22a5770003d8857a4bc8f5407e6cd5d90e
Adish <adishkerkar@gmail.com>
2023-09-20 17:02:56 +0530

    added basic functionality to book

--- src/components/Booking/BookingHome.jsx
+++ src/components/Booking/BookingHome.jsx
@@ -6,7 +6,7 @@ import './BookingHome.css'
 import { useEffect } from 'react';
 
 import Bookingprovider from './Bookingprovider';
-import { SERVICES_DATA, BASE_URL } from '../apiUrls';
+import { BASE_URL } from '../apiUrls';
 import axios from "axios";
 import { useParams } from 'react-router-dom';
 import BookingContextProvider from './BookingContextProvider';
@@ -17,27 +17,9 @@ function BookingHome() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const providerparams = searchParams.get('provider');
-  const serviceparams = searchParams.get('service');
-  const [servicedata, setServicedata] = useState(null);
+  const serviceName = searchParams.get("service_name");
   const [providerdata, setProviderdata] = useState(null);
-
   const id = useParams();
-
-  useEffect(() => {
-    let getservicesdata = async () => {
-      let servicedata = await axios.get(SERVICES_DATA);
-      return servicedata.data.service_owners;
-    }
-    getservicesdata().then((data) => {
-
-      let filterservice = data.find((elem) => {
-        return elem.id == id.id
-      });
-      let servicename = filterservice.services.find((service) => service.id == serviceparams);
-      setServicedata(servicename.name);
-    });
-
-  }, [])
 
   useEffect(() => {
     async function getServiceProviders() {
@@ -62,7 +44,7 @@ function BookingHome() {
         <>
           <BookingContextProvider>
           <BookinCalender providerProperties={providerdata} />
-          <span id='SelectedServiceBooking'>{servicedata}</span>
+          <span id='SelectedServiceBooking'>{serviceName}</span>
           <BookingTime providerProperties={providerdata} />
           <Bookingprovider providerProperties={providerdata} />
           <Booking_Submit/>
--- src/components/Booking/Booking_Submit.jsx
+++ src/components/Booking/Booking_Submit.jsx
@@ -1,23 +1,54 @@
 import React from 'react'
 import { useBookingContext } from './BookingContextProvider'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 
 function Booking_Submit() {
-  
+
   const location = useLocation();
-  const {selectedTimeslot} = useBookingContext(); 
+  const { selectedTimeslot } = useBookingContext();
   const queryParams = new URLSearchParams(location.search);
   const providerId = queryParams.get("provider_id");
+  const serviceName = queryParams.get("service_name");
+  const service_owner_id = useParams().id;
+  let slot_indexes = selectedTimeslot.map((slot)=>{
+    return  slot.slot_index
+  })
+  // let duration = selectedTimeslot.map((slot=>{
+  //     duration
+  // }))
 
-  function handleBooking(){
+
+  function handleBooking() {
+    const date = new Date();
+    let bookingData = {
+      service_provider_id: providerId,
+      service_owner_id: service_owner_id,
+      user_id: "1",
+      service_date_time: date,
+      created_date_time: date,
+      updated_date_time: date,
+      slot_indexes: slot_indexes,
+      booking_status: "Pending",
+      services: [
+        {
+          id: "HAIRCUT",
+          name: "Hair Cut",
+          duration: "30"
+        }
+      ]
+    }
+    console.log(bookingData);
+    console.log(date);
     console.log(providerId);
-     console.log(selectedTimeslot);
+    console.log(serviceName);
+    console.log(selectedTimeslot);
   }
+
   return (
     <>
-            <div id="bookingBtnContainer">
-            <button id='bookingControllerBtn' onClick={handleBooking}>Continue</button>
-            </div>
+      <div id="bookingBtnContainer">
+        <button id='bookingControllerBtn' onClick={handleBooking}>Continue</button>
+      </div>
     </>
   )
 }
